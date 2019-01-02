8e8563118a36e7efc8f4a23ccc20dc6b84d58916
Leonardo camilo ribeiro <leo.camilo.ribeiro@gmail.com>
2019-01-02 19:43:26 -0200

    improving code and making more quizzes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import csv
 import numpy as np
+import matplotlib.pyplot as plt
 from datetime import datetime as dt
 from collections import defaultdict
 from functools import reduce
@@ -37,6 +38,7 @@ for e in enrollments:
 for engagement in daily_engagement:
 	engagement['account_key'] = parse_maybe_int(engagement['acct'])
 	engagement['lessons_completed']	= float(engagement['lessons_completed'])
+	engagement['num_courses_visited'] = float(engagement['num_courses_visited'])
 	engagement['total_minutes_visited']	= float(engagement['total_minutes_visited'])
 	engagement['utc_date'] = parse_date(engagement['utc_date'])
 	engagement_users_set.add(engagement['account_key'])
@@ -130,6 +132,14 @@ for e in paid_engagements:
 print(' paid_engagement_in_first_week: {0}'.format(len(paid_engagement_in_first_week)))
 print('\n\n============================\n\n')
 
+def group_data_by_acc(columm, data):
+	data_by_account = defaultdict(list)
+
+	for d in data:
+		data_by_account[d[columm]].append(d)
+
+	return data_by_account
+
 def get_total_columm_value(columm, data):
 	total_data_by_account = {}
 	
@@ -139,10 +149,13 @@ def get_total_columm_value(columm, data):
 
 	return total_data_by_account
 
-engagement_by_account = defaultdict(list)
+def show_metrics(description, data_list):
+	print(' {0} [mean]: {1}'.format(description, np.mean(data_list)))
+	print(' {0} [minimum]: {1}'.format(description, np.min(data_list)))
+	print(' {0} [maximum]: {1}'.format(description, np.max(data_list)))
+	print(' {0} [standart deviation]: {1}'.format(description, np.std(data_list)))
 
-for e in paid_engagement_in_first_week:
-	engagement_by_account[e['account_key']].append(e)
+engagement_by_account = group_data_by_acc('account_key', paid_engagement_in_first_week)
 
 total_minutes_by_account = get_total_columm_value('total_minutes_visited', engagement_by_account)
 total_lessons_by_account = get_total_columm_value('lessons_completed', engagement_by_account)
@@ -150,16 +163,70 @@ total_lessons_by_account = get_total_columm_value('lessons_completed', engagemen
 total_minutes_by_account_list = list(total_minutes_by_account.values())
 total_lessons_by_account_list = list(total_lessons_by_account.values())
 
-print(' total_minutes [mean]: {0}'.format(np.mean(total_minutes_by_account_list)))
-print(' total_minutes [standart deviation]: {0}'.format(np.std(total_minutes_by_account_list)))
-print(' total_minutes [minimum]: {0}'.format(np.min(total_minutes_by_account_list)))
-print(' total_minutes [maximum]: {0}'.format(np.max(total_minutes_by_account_list)))
+show_metrics('total_minutes', total_minutes_by_account_list)
 
 print('\n\n============================\n\n')
 
-print(' total_lessons [mean]: {0}'.format(np.mean(total_lessons_by_account_list)))
-print(' total_lessons [standart deviation]: {0}'.format(np.std(total_lessons_by_account_list)))
-print(' total_lessons [minimum]: {0}'.format(np.min(total_lessons_by_account_list)))
-print(' total_lessons [maximum]: {0}'.format(np.max(total_lessons_by_account_list)))
+show_metrics('total_lessons', total_lessons_by_account_list)
 
 print('\n\n============================\n\n')
+
+def get_total_unique_columm_value(columm, data):
+	total_data_by_account = {}
+	
+	for acck, data_list in data.items():
+		just_engagement_values = list(map((lambda e: 1 if e[columm] > 0 else 0), data_list))
+		total_data_by_account[acck] = reduce((lambda agg, data_col: agg + data_col), just_engagement_values)
+
+	return total_data_by_account
+
+num_courses_visited = get_total_unique_columm_value('num_courses_visited', engagement_by_account)
+total_num_courses_visited = list(num_courses_visited.values())
+
+show_metrics('num_courses_visited', total_num_courses_visited)
+
+print('\n\n============================\n\n')
+
+subway_project_lesson_keys = ['746169184', '3176718735']
+project_passing_keys = ['PASSED', 'DISTINCTION']
+pass_subway_project_by_user = set()
+non_passing_engagement = []
+passing_engagement = []
+
+for submission in paid_submissions:
+	if (submission['lesson_key'] in subway_project_lesson_keys) and (submission['assigned_rating'] in project_passing_keys):
+		pass_subway_project_by_user.add(submission['account_key'])
+
+for engagement in paid_engagement_in_first_week:
+	if engagement['account_key'] in pass_subway_project_by_user:
+		passing_engagement.append(engagement)
+	else:
+		non_passing_engagement.append(engagement)
+
+print(' passing_engagement: {0}'.format(len(passing_engagement)))
+print(' non_passing_engagement: {0}'.format(len(non_passing_engagement)))
+
+print('\n\n============================\n\n')
+
+passing_engagement_by_account = group_data_by_acc('account_key', passing_engagement)
+non_passing_engagement_by_account = group_data_by_acc('account_key', non_passing_engagement)
+
+total_minutes_by_account_pass = get_total_columm_value('total_minutes_visited', passing_engagement_by_account)
+lessons_completed_by_account_pass = get_total_columm_value('lessons_completed', passing_engagement_by_account)
+total_minutes_by_account_non_pass = get_total_columm_value('total_minutes_visited', non_passing_engagement_by_account)
+lessons_completed_by_account_non_pass = get_total_columm_value('lessons_completed', non_passing_engagement_by_account)
+
+total_minutes_by_account_pass_list = list(total_minutes_by_account_pass.values())
+total_minutes_by_account_non_pass_list = list(total_minutes_by_account_non_pass.values())
+lessons_completed_by_account_pass_list = list(lessons_completed_by_account_pass.values())
+lessons_completed_by_account_non_pass_list = list(lessons_completed_by_account_non_pass.values())
+
+show_metrics('total_minutes passing studants', total_minutes_by_account_pass_list)
+show_metrics('total_minutes non passing studants', total_minutes_by_account_non_pass_list)
+show_metrics('lessons_completed passing studants', lessons_completed_by_account_pass_list)
+show_metrics('lessons_completed non passing studants', lessons_completed_by_account_non_pass_list)
+
+print('\n\n============================\n\n')
+
+plt.hist(lessons_completed_by_account_non_pass_list)
+plt.show()
